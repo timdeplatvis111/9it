613a98560d37a32f28cfd4a7117320a3bb68ab3c
timdeplatvis111 <timdeplatvis@gmail.com>
2018-09-27 10:56:13 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html lang="nl">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="beschrijving">
+    <meta name="author" content="Auteur">
+    <title>9dit</title>
+    <link rel="stylesheet" href="">
+
+
+    <body>
+
+
+    
+    </body>
